e167082828f571f725a05d3beb90d7ceeeb60b89
andrew <andrew.taggart@colorado.edu>
2012-02-20 20:07:17 -0700

    updated c# project files

---
+++ lol-itemsets-lib/ItemSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Web.Script.Serialization;
+
+namespace lol_itemsets_lib
+{
+    using System;
+    using System.Resources;
+    using System.IO;
+
+    public class ItemSet
+    {
+        private static ItemSet instance;
+        private List<Item> items;
+
+        private ItemSet()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            String json = File.ReadAllText(@"Data/items.json");
+            items = ser.Deserialize<List<Item>>(json);
+        }
+
+        public static ItemSet Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ItemSet();
+                }
+                return instance;
+            }
+        }
+    }
+}
