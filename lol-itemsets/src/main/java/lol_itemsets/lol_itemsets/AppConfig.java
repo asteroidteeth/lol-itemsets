dbc6a6e6c87d5ac015233b6800d1273a71871ada
andrew <justhurryup@justhurryup-PC.(none)>
2012-02-27 19:57:11 -0700

    Added toString method for ChampData for the sake of the champion selection box. Added configuration code for the champion ini directories.

--- lol-itemsets/src/main/java/lol_itemsets/lol_itemsets/AppConfig.java
+++ lol-itemsets/src/main/java/lol_itemsets/lol_itemsets/AppConfig.java
@@ -1,6 +1,8 @@
 package lol_itemsets.lol_itemsets;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class AppConfig {
 
@@ -9,16 +11,23 @@ public class AppConfig {
 	private final String itemImgDir = "items";
 	private final String champListFile = "champ-list.json";
 	private final String champImgDir = "champs";
-	
-	public AppConfig() { 
+	private String loldir;
+	private final String releasesDir = "RADS\\solutions\\lol_game_client_sln\\releases";
+	private final String configDirTemplate = "RADS\\solutions\\lol_game_client_sln\\releases\\%s\\deploy\\DATA\\Characters";
+	private File configDir;
+	private final String classicIniFileName = "RecItemsCLASSIC.ini";
+	private final String domIniFileName = "RecItemsODIN.ini";
+
+	public AppConfig() {
 		String prop = System.getProperty("itemset.data");
-		dataDir = (prop != null) ? prop : "./data"; 
+		dataDir = (prop != null) ? prop : "./data";
+		setLoldir("C:\\Riot Games\\League of Legends");
 	}
-	
+
 	public File getDataDir() {
 		return new File(dataDir);
 	}
-	
+
 	public File getItemList() {
 		return new File(dataDir, itemListFile);
 	}
@@ -33,6 +42,54 @@ public class AppConfig {
 
 	public File getChampImageDirectory() {
 		return new File(dataDir, champImgDir);
+	}
+
+	public String getLoldir() {
+		return loldir;
+	}
+
+	public File getClassicChampIni(String champName) {
+		File dir = new File(configDir, champName);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		return new File(dir, classicIniFileName);
+
+	}
+
+	public File getDominionChampIni(String champName) {
+		File dir = new File(configDir, champName);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		return new File(dir, domIniFileName);
+
+	}
+
+	public void setLoldir(String loldir) {
+		this.loldir = loldir;
+		String version = getLatestVersion(new File(loldir, releasesDir));
+		configDir = new File(loldir,String.format(configDirTemplate, version));
+	}
+
+	private String getLatestVersion(File dir) {
+		List<File> files = Arrays.asList(dir.listFiles());
+		File latest = files.get(0);
+		int latestVal = Integer.parseInt(latest.getName().split("\\.")[3]);
+		for (File f : files.subList(1, files.size())) {
+			int testVal = Integer.parseInt(f.getName().split("\\.")[3]);
+			if (testVal > latestVal) {
+				latest = f;
+				latestVal = testVal;
+			}
+		}
+
+		return latest.getName();
+	}
+
+	public static void main(String[] args) {
+		AppConfig c = new AppConfig();
+		System.out.println(c.getClassicChampIni("Annie"));
 	}
 
 }
--- lol-itemsets/src/main/java/lol_itemsets/lol_itemsets/ChampData.java
+++ lol-itemsets/src/main/java/lol_itemsets/lol_itemsets/ChampData.java
@@ -35,4 +35,9 @@ public class ChampData {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }
